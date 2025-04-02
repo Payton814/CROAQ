78db7065d66448627bcd31529e738447e85ebba7
Payton PC <lintonpayton99@gmail.com>
2025-04-01 20:56:26 -0400

    Now CROAQ.sh takes an arguemnt which is the data directory. It will then create a directory inside the data directory for the data of this specific run. It will overwrite an existing directory if it already exists so be careful. This is mainly done for testing

--- CROAQ.sh
+++ CROAQ.sh
@@ -4,7 +4,10 @@ xdg-open ./logo/Croaqcomt1.png
 sleep 5
 pkill eog
 
-cp -f ./template/dtmp.csv ./data/trial1.csv
+DATA_DIR=$1
+
+rm -r ./data/$DATA_DIR; mkdir ./data/$DATA_DIR
+cp -f ./template/dtmp.csv ./data/$DATA_DIR/trial1.csv
 
 echo "Sample Measurement in Progress..."
 CURRENT_POS=0 ## Initialize actuator position to 0
@@ -12,7 +15,7 @@ NSTEPS=2 ## Number of times the actuator will move sample
 STEP_SIZE=500 ## The step size for the actuator to make
                ## The development was done with Actuonix P8-75-165-3-ST (Each 1 step is ~0.0018mm)
 python CROAQ.py $CURRENT_POS ## Take sample measurement. This is the empty cavity measurement
-python updatePlot.py ./data/trial1.csv &
+python updatePlot.py ./data/$DATA_DIR/trial1.csv &
 
 
 while [ $CURRENT_POS -lt $NSTEPS ]
