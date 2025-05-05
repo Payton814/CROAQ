bb39f96953fbf86fcba5cda744aadf1fb3f38e58
Payton_PI5 <lintonpayton99@gmail.com>
2025-05-05 16:43:20 -0400

    some fixes and test with Q. Seems to work fine. But legend is being a bith

--- updatePlot.py
+++ updatePlot.py
@@ -18,15 +18,17 @@ def animate(i, FILE = FILE):
     x = range(len(y))
     ax1.cla()
     ax2.cla()
-    ax1.plot(x, y, marker = 'o', color = 'darkolivegreen')
-    ax2.plot(x, Qraw, marker = 'o', color = 'cornflowerblue')
-    ax2.plot(x, Qspline, marker = 'D', color = 'darkcyan')
+    ax1.plot(x, y, marker = 'o', color = 'darkolivegreen', label = 'Resonant Frequency')
+    ax2.plot(x, Qraw, marker = 'o', color = 'cornflowerblue', label = 'Q raw')
+    ax2.plot(x, Qspline, marker = 'D', color = 'darkcyan', label = 'Q spline')
     ax1.set_ylim(0.999*y.min(), 1.001*y.max())
     ax2.set_ylim(0.8*Qraw.min(), 6000)
 
 fig, ax1 = plt.subplots()
-ax1.grid()
+
 ax2 = ax1.twinx()
+plt.legend()
+#plt.grid()
 
 ani = FuncAnimation(plt.gcf(), animate, interval=5000, cache_frame_data=False)
 plt.show()
