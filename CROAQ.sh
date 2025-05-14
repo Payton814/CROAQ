fa4760bc6d00715305aba90c0601247b32f02d6e
Payton_PI5 <lintonpayton99@gmail.com>
2025-05-14 10:27:11 -0400

    Slowed down actuator. Seems it might not have been moving as far as it said? unsure. Did Rexolite Rod3 Peak1 for test

--- CROAQ.sh
+++ CROAQ.sh
@@ -12,7 +12,7 @@ cp -f ./template/dtmp.csv ./data/$DATA_DIR/trial1.csv
 
 echo "Sample Measurement in Progress..."
 CURRENT_POS=0 ## Initialize actuator position to 0
-NSTEPS=70 ## Number of times the actuator will move sample
+NSTEPS=78 ## Number of times the actuator will move sample
 STEP_SIZE=250 ## The step size for the actuator to make
                ## The development was done with Actuonix P8-75-165-3-ST (Each 1 step is ~0.0018mm)
 python CROAQ.py $CURRENT_POS $DATA_DIR $PEAK ## Take sample measurement. This is the empty cavity measurement
@@ -29,7 +29,7 @@ do
     ##CURRENT_POS=$((CURRENT_POS+1))
 done
 
-echo "Retracting $((CURRENT_POS*250)) steps..."
-ticcmd --exit-safe-start --position-relative -$((CURRENT_POS*250))
+echo "Retracting $((CURRENT_POS*STEP_SIZE)) steps..."
+ticcmd --exit-safe-start --position-relative -$((CURRENT_POS*STEP_SIZE))
 
 echo "Sample Measurement Complete"
