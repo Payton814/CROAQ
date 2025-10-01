762eaff324a910c59ba3214b0b7bd661754bce06
Payton_PI5 <lintonpayton99@gmail.com>
2025-10-01 14:17:21 -0400

    First lunar sample measurement

--- CROAQ.py
+++ CROAQ.py
@@ -26,11 +26,11 @@ import sys
 CURRENT_POS = float(sys.argv[1])
 #IP = "192.168.2.233" ## IP address for the network analyzer in Beatty lab
 IFBandwidth = 1000 ## In Hz
-WINDOW_WIDTH = 1.0e7
+WINDOW_WIDTH = 5.0e6
 if (CURRENT_POS == 0):
     delf = 0
     fcent = findPeak(int(sys.argv[3]))
-    fcent = findPeak(1, fl = fcent - 100e6, fu = fcent + 100e6, height = -20)
+    fcent = findPeak(1, fl = fcent - 100e6, fu = fcent + 100e6, height = -30)
 else:
     df = pd.read_csv('./data/' + sys.argv[2] + '/trial1.csv')
     flast = np.array(df['Frequency (GHz)'])[-1] ## Grab the resonant frequency fro last measurement
--- CROAQ.sh
+++ CROAQ.sh
@@ -12,7 +12,7 @@ cp -f ./template/dtmp.csv ./data/$DATA_DIR/trial1.csv
 
 echo "Sample Measurement in Progress..."
 CURRENT_POS=0 ## Initialize actuator position to 0
-NSTEPS=65 ## Number of times the actuator will move sample
+NSTEPS=70 ## Number of times the actuator will move sample
 STEP_SIZE=250 ## The step size for the actuator to make
                ## The development was done with Actuonix P8-75-165-3-ST (Each 1 step is ~0.0018mm)
 python CROAQ.py $CURRENT_POS $DATA_DIR $PEAK ## Take sample measurement. This is the empty cavity measurement
