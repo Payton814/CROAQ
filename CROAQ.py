0a0bcb4f1b45735babce70cef6a844f0b9e652ff
Payton_PI5 <lintonpayton99@gmail.com>
2025-10-15 19:05:01 -0400

    AcrylicSolid_uwave

--- CROAQ.py
+++ CROAQ.py
@@ -30,7 +30,7 @@ WINDOW_WIDTH = 5.0e6
 if (CURRENT_POS == 0):
     delf = 0
     fcent = findPeak(int(sys.argv[3]))
-    fcent = findPeak(1, fl = fcent - 100e6, fu = fcent + 100e6, height = -30)
+    fcent = findPeak(1, fl = fcent - 100e6, fu = fcent + 100e6, height = -40)
 else:
     df = pd.read_csv('./data/' + sys.argv[2] + '/trial1.csv')
     flast = np.array(df['Frequency (GHz)'])[-1] ## Grab the resonant frequency fro last measurement
@@ -70,8 +70,8 @@ df.to_csv('./data/' + sys.argv[2] + '/' + sys.argv[1] + '.csv', index=False, hea
 
 fres = f[S21.argmax()]
 print('Resonant Frequency', fres/1e9, "GHz")
-Data2 = {'Step': [CURRENT_POS*250],
-         'height (mm)': [CURRENT_POS*250*0.0018],
+Data2 = {'Step': [CURRENT_POS*75],
+         'height (mm)': [CURRENT_POS*75*0.006],
          'Frequency (GHz)': [fres/1e9],
          'Qspline': [Qspline],
          'Qraw': [Qraw]}
