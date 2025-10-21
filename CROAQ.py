a277e3d68bc252f4b265df510a6d7fdcd2db5709
Payton_PI5 <lintonpayton99@gmail.com>
2025-10-20 21:41:36 -0400

    LunarSample2 peak 4

--- CROAQ.py
+++ CROAQ.py
@@ -30,7 +30,7 @@ WINDOW_WIDTH = 5.0e6
 if (CURRENT_POS == 0):
     delf = 0
     fcent = findPeak(int(sys.argv[3]))
-    fcent = findPeak(1, fl = fcent - 50e6, fu = fcent + 50e6, height = -20)
+    fcent = findPeak(1, fl = fcent - 50e6, fu = fcent + 50e6, height = -40)
 else:
     df = pd.read_csv('./data/' + sys.argv[2] + '/trial1.csv')
     flast = np.array(df['Frequency (GHz)'])[-1] ## Grab the resonant frequency fro last measurement
