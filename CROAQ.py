c7b3c07bdfbb78dc88db4c05e338969111ea8f9b
Payton_PI5 <lintonpayton99@gmail.com>
2025-10-22 13:12:08 -0400

    rex

--- CROAQ.py
+++ CROAQ.py
@@ -26,11 +26,11 @@ import sys
 CURRENT_POS = float(sys.argv[1])
 #IP = "192.168.2.233" ## IP address for the network analyzer in Beatty lab
 IFBandwidth = 1000 ## In Hz
-WINDOW_WIDTH = 5.0e6
+WINDOW_WIDTH = 10.0e6
 if (CURRENT_POS == 0):
     delf = 0
     fcent = findPeak(int(sys.argv[3]))
-    fcent = findPeak(1, fl = fcent - 100e6, fu = fcent + 100e6, height = -40)
+    fcent = findPeak(1, fl = fcent - 100e6, fu = fcent + 100e6, height = -35)
 else:
     df = pd.read_csv('./data/' + sys.argv[2] + '/trial1.csv')
     flast = np.array(df['Frequency (GHz)'])[-1] ## Grab the resonant frequency fro last measurement
