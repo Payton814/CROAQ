2878d020d635dff5692c3e05e1d6ab60c3574e38
Payton_PI5 <lintonpayton99@gmail.com>
2025-10-22 16:48:02 -0400

    peak 3

--- CROAQ.sh
+++ CROAQ.sh
@@ -16,17 +16,17 @@ PEAK8=$9
 
 for PEAK in $PEAK1 $PEAK2 $PEAK3 $PEAK4 $PEAK5 $PEAK6 $PEAK7 $PEAK8
 do
-rm -r ./data/$DATA_DIR$PEAK; mkdir ./data/$DATA_DIR$PEAK
-cp -f ./template/dtmp.csv ./data/$DATA_DIR$PEAK/trial1.csv
+rm -r ./data/$DATA_DIR$((PEAK+1)); mkdir ./data/$DATA_DIR$((PEAK+1))
+cp -f ./template/dtmp.csv ./data/$DATA_DIR$((PEAK+1))/trial1.csv
 
 echo "Sample Measurement in Progress..."
 CURRENT_POS=0 ## Initialize actuator position to 0
 NSTEPS=74 ## Number of times the actuator will move sample
 STEP_SIZE=150 ## The step size for the actuator to make
                ## The development was done with Actuonix P8-75-165-3-ST (Each 1 step is ~0.0018mm)
-python CROAQ.py $CURRENT_POS $DATA_DIR$PEAK $PEAK ## Take sample measurement. This is the empty cavity measurement
+python CROAQ.py $CURRENT_POS $DATA_DIR$((PEAK+1)) $PEAK ## Take sample measurement. This is the empty cavity measurement
 sleep 10
-python updatePlot.py ./data/$DATA_DIR$PEAK/trial1.csv &
+python updatePlot.py ./data/$DATA_DIR$((PEAK+1))/trial1.csv &
 
 
 while [ $CURRENT_POS -lt $NSTEPS ]
@@ -35,7 +35,7 @@ do
     ticcmd --exit-safe-start --position-relative $STEP_SIZE ## Move the actuator by the STEP_SIZE
     CURRENT_POS=$((CURRENT_POS+1))
     sleep 8 ## Wait before executing next command to be sure the actuator has stopped moving
-    python CROAQ.py $CURRENT_POS $DATA_DIR$PEAK $PEAK
+    python CROAQ.py $CURRENT_POS $DATA_DIR$((PEAK+1)) $PEAK
     ##CURRENT_POS=$((CURRENT_POS+1))
 done
 
