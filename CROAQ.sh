a66e10e7d20d07b0b43e2e73c1d7e03e52719abd
Payton_PI5 <lintonpayton99@gmail.com>
2025-10-22 03:31:18 -0400

    WR159 Rexolite Peaks 2 and 3

--- CROAQ.sh
+++ CROAQ.sh
@@ -10,16 +10,19 @@ PEAK2=$3
 PEAK3=$4
 PEAK4=$5
 PEAK5=$6
+PEAK6=$7
+PEAK7=$8
+PEAK8=$9
 
-for PEAK in $PEAK1 $PEAK2 $PEAK3 $PEAK4 $PEAK5
+for PEAK in $PEAK1 $PEAK2 $PEAK3 $PEAK4 $PEAK5 $PEAK6 $PEAK7 $PEAK8
 do
 rm -r ./data/$DATA_DIR$PEAK; mkdir ./data/$DATA_DIR$PEAK
 cp -f ./template/dtmp.csv ./data/$DATA_DIR$PEAK/trial1.csv
 
 echo "Sample Measurement in Progress..."
 CURRENT_POS=0 ## Initialize actuator position to 0
-NSTEPS=72 ## Number of times the actuator will move sample
-STEP_SIZE=250 ## The step size for the actuator to make
+NSTEPS=74 ## Number of times the actuator will move sample
+STEP_SIZE=150 ## The step size for the actuator to make
                ## The development was done with Actuonix P8-75-165-3-ST (Each 1 step is ~0.0018mm)
 python CROAQ.py $CURRENT_POS $DATA_DIR$PEAK $PEAK ## Take sample measurement. This is the empty cavity measurement
 sleep 10
